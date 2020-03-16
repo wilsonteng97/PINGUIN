d2a01fa04d1654aca4176336c0db23032fed22b0
kellypak <61049586+kellypak@users.noreply.github.com>
2020-03-16 17:23:15 +0800

    Update MapsActivity.java

--- FitFlop/app/src/main/java/com/cz2006/fitflop/ui/screens/MapsActivity.java
+++ FitFlop/app/src/main/java/com/cz2006/fitflop/ui/screens/MapsActivity.java
@@ -47,9 +47,12 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     public void onMapReady(GoogleMap googleMap) {
         // Add a marker in Sydney, Australia,
         // and move the map's camera to the same location.
-        LatLng sydney = new LatLng(-33.852, 151.211);
-        googleMap.addMarker(new MarkerOptions().position(sydney)
-                .title("Marker in Sydney"));
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        //LatLng sydney = new LatLng(-33.852, 151.211);
+        //googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        //googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        
+        // Display Singapore :D
+        LatLngBounds Singapore = new LatLngBounds(new LatLng(1.27274, 103.602552), new LatLng(1.441715, 104.039828));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(Singapore.getCenter(), 10));
     }
 }
