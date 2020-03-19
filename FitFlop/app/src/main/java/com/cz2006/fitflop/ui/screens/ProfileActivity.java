e8c5b24f49bf4a713377a1956291501b2fb27443
Wilson <wilsonteng89@gmail.com>
2020-03-19 14:38:32 +0800

    Auto stash before merge of "master" and "origin/master"

--- FitFlop/app/src/main/java/com/cz2006/fitflop/ui/screens/ProfileActivity.java
+++ FitFlop/app/src/main/java/com/cz2006/fitflop/ui/screens/ProfileActivity.java
@@ -2,35 +2,45 @@ package com.cz2006.fitflop.ui.screens;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import com.cz2006.fitflop.R;
 
 import static com.cz2006.fitflop.R.layout.activity_profiles;
 
-public class ProfileActivity extends AppCompatActivity {
+public class ProfileActivity extends Fragment {
 
-    LinearLayout dynamicContent, bottomNavBar;
-
+    @Nullable
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.base_activity);
-
-        dynamicContent = (LinearLayout) findViewById(R.id.dynamicContent);
-        bottomNavBar = (LinearLayout) findViewById(R.id.bottomNavBar);
-        View wizard = getLayoutInflater().inflate(activity_profiles, null);
-        dynamicContent.addView(wizard);
-
-        RadioGroup rg = (RadioGroup) findViewById(R.id.radioGroup1);
-        RadioButton rb = (RadioButton) findViewById(R.id.Profile);
-
-        rb.setCompoundDrawablesWithIntrinsicBounds( 0,R.drawable.ic_profile_black_selected_24dp, 0,0);
-        rb.setTextColor(Color.parseColor("#3F51B5"));
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(activity_profiles, container, false);
     }
+//    LinearLayout dynamicContent, bottomNavBar;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.base_activity);
+//
+//        dynamicContent = (LinearLayout) findViewById(R.id.dynamicContent);
+//        bottomNavBar = (LinearLayout) findViewById(R.id.bottomNavBar);
+//        View wizard = getLayoutInflater().inflate(activity_profiles, null);
+//        dynamicContent.addView(wizard);
+//
+//        RadioGroup rg = (RadioGroup) findViewById(R.id.radioGroup1);
+//        RadioButton rb = (RadioButton) findViewById(R.id.Profile);
+//
+//        rb.setCompoundDrawablesWithIntrinsicBounds( 0,R.drawable.ic_profile_black_selected_24dp, 0,0);
+//        rb.setTextColor(Color.parseColor("#3F51B5"));
+//    }
 }
