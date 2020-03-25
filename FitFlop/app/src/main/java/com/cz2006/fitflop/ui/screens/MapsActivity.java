e16389afbe3ca76390c5644d271730a4de60a397
Wilson <wilsonteng89@gmail.com>
2020-03-26 02:50:27 +0800

    Refactored Code to uphold Single-responsibility principle
    * Single-responsibility principle from SOLID design principles.
    * Moved map distance calculation to logic package.

--- FitFlop/app/src/main/java/com/cz2006/fitflop/ui/screens/MapsActivity.java
+++ FitFlop/app/src/main/java/com/cz2006/fitflop/ui/screens/MapsActivity.java
@@ -56,11 +56,12 @@ import java.util.Set;
 import java.util.Arrays;
 
 import static com.cz2006.fitflop.R.layout.activity_maps;
+import static com.cz2006.fitflop.logic.MapDistanceLogic.*;
+
 
 public class MapsActivity extends Fragment implements OnMapReadyCallback, LocationListener {
 
     private static final String TAG = "MapsActivity";
-    private static final double EARTH_RADIUS = 6371000; //meters
 
     private String apiPlacesKey; // Instantiated in OnCreate
     PlacesClient placesClient;
@@ -76,9 +77,6 @@ public class MapsActivity extends Fragment implements OnMapReadyCallback, Locati
     private static SeekBar seekBar;
     private static TextView progressShow;
     private static BitmapDescriptor pointIcon;
-
-
-
 
     @Nullable
     @Override
@@ -115,7 +113,7 @@ public class MapsActivity extends Fragment implements OnMapReadyCallback, Locati
                     near_layer.setOnFeatureClickListener(new GeoJsonLayer.OnFeatureClickListener() {
                         @Override
                         public void onFeatureClick(Feature feature) {
-                            Log.e(TAG, "Feature clicked: " + feature.getProperty("NAME"));
+                            Log.i(TAG, "Feature clicked: " + feature.getProperty("NAME"));
                         }
                     });
                 } catch (IOException e) {
@@ -267,7 +265,6 @@ public class MapsActivity extends Fragment implements OnMapReadyCallback, Locati
         }
     }
 
-
     private LatLng getLatLngFromGeoJsonFeature(GeoJsonFeature feature) {
         String[] coord_str = feature.getGeometry().getGeometryObject().toString().split(",");
         String lat = coord_str[0].substring(10);
@@ -303,70 +300,6 @@ public class MapsActivity extends Fragment implements OnMapReadyCallback, Locati
         return isNear;
     }
 
-    private double getDistance(double metres, LatLng feature_location, LatLng current_user_location) {
-        double dist = 0;
-        if (metres <= 100) {
-            dist = Math.abs(getUnaccurateDist(feature_location, current_user_location));
-        } else {
-            dist = Math.abs(getAccurateDist(feature_location, current_user_location));
-        }
-        return dist;
-    }
-
-    /**
-     * Inaccurate for distances more than 100m but very fast. (Assumes the world is Flat)
-     * Works by approximating the distance of a single latitude/longitude at the given latitude and
-     *
-     * @param latLng1
-     * @param latLng2
-     * @returns Pythagorean distance in metres
-     */
-    public double getUnaccurateDist(LatLng latLng1, LatLng latLng2) {
-        double lat1 = latLng1.latitude;
-        double lng1 = latLng1.longitude;
-        double lat2 = latLng2.latitude;
-        double lng2 = latLng2.longitude;
-
-        double a = (lat1 - lat2) * distPerLat(lat1);
-        double b = (lng1 - lng2) * distPerLng(lat1);
-        return Math.sqrt(a * a + b * b);
-    }
-
-    private static double distPerLng(double lat) {
-        return 0.0003121092 * Math.pow(lat, 4)
-                + 0.0101182384 * Math.pow(lat, 3)
-                - 17.2385140059 * lat * lat
-                + 5.5485277537 * lat + 111301.967182595;
-    }
-
-    private static double distPerLat(double lat) {
-        return -0.000000487305676 * Math.pow(lat, 4)
-                - 0.0033668574 * Math.pow(lat, 3)
-                + 0.4601181791 * lat * lat
-                - 1.4558127346 * lat + 110579.25662316;
-    }
-
-
-    private double getAccurateDist(LatLng latLng1, LatLng latLng2) {
-        double lat1 = latLng1.latitude;
-        double lng1 = latLng1.longitude;
-        double lat2 = latLng2.latitude;
-        double lng2 = latLng2.longitude;
-
-        double latDistance = Math.toRadians(lat2 - lat1);
-        double lonDistance = Math.toRadians(lng2 - lng1);
-        double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2)
-                + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2))
-                * Math.sin(lonDistance / 2) * Math.sin(lonDistance / 2);
-        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-        double distance = EARTH_RADIUS * c;
-
-//        double height = h1 - h2;
-//        distance = Math.pow(distance, 2) + Math.pow(height, 2);
-
-        distance = Math.pow(distance, 2);
-        return Math.sqrt(distance);
-    }
     public void searchPlaces(final GoogleMap googleMap){
         if (!Places.isInitialized()){
             Places.initialize(getActivity().getApplicationContext(), apiPlacesKey);
