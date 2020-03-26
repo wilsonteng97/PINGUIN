50b4a9d4e28f9d1114f0c267b8dceba5ccaeb12b
kellypak <kelly_pakxl@yahoo.com.sg>
2020-03-26 15:53:28 +0800

    information layout edits

--- FitFlop/app/src/main/java/com/cz2006/fitflop/ui/GeoJsonFeatureInfoActivity.java
+++ FitFlop/app/src/main/java/com/cz2006/fitflop/ui/GeoJsonFeatureInfoActivity.java
@@ -1,8 +1,57 @@
 package com.cz2006.fitflop.ui;
 
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.cz2006.fitflop.R;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class GeoJsonFeatureInfoActivity extends AppCompatActivity {
 
+    String GymName, Description, StreetName, BuildingName, BlockNumber, FloorNumber, UnitNumber, PostalCode;
+    TextView name, description, street, building, block, floor, unit, postal;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // FIXME: Pass in the data from GeoJson here instead of hardcoding
+        GymName = "OMG Yoga";
+        Description = "Operating Hours: Monday & Friday: 10.00am - 6.00pm";
+        StreetName = "Jurong Gateway Road";
+        BuildingName = "-";
+        BlockNumber = "134";
+        FloorNumber = "4";
+        UnitNumber = "309";
+        PostalCode = "600134";
+
+        // Initialise Views
+        name = findViewById(R.id.name);
+        description = findViewById(R.id.description);
+        street = findViewById(R.id.street);
+        building = findViewById(R.id.building);
+        block = findViewById(R.id.block);
+        floor = findViewById(R.id.floor);
+        unit = findViewById(R.id.unit);
+        postal = findViewById(R.id.postal);
+
+        inputInformationIntoLayout();
+
+    }
+
+    public void inputInformationIntoLayout(){
+        name.setText(GymName);
+        description.setText(Description);
+        street.setText(StreetName);
+        building.setText(BuildingName);
+        block.setText(BlockNumber);
+        floor.setText(FloorNumber);
+        unit.setText(UnitNumber);
+        postal.setText(PostalCode);
+    }
 
 }
+
+
