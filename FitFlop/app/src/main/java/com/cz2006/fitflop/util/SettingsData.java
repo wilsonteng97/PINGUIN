478c20cd4dacfe2a48143a5a6082a7731e69273c
boonshuan <48539225+boonshuan@users.noreply.github.com>
2020-03-26 21:09:21 +0800

    Settings by Boon v1

---
+++ FitFlop/app/src/main/java/com/cz2006/fitflop/util/SettingsData.java
@@ -0,0 +1,22 @@
+package com.cz2006.fitflop.util;
+
+import com.cz2006.fitflop.R;
+
+public class SettingsData {
+
+    public static String[] title = new String[] {"Facebook","Twitter","Instagram"
+    };
+
+    public static String[] description = new String[] {
+            "Connect to Facebook",
+            "Connect to Twitter",
+            "Connect to Instagram"
+    };
+
+    public static int[] picturePath = new int[] {
+            R.drawable.facebook,
+            R.drawable.twitter,
+            R.drawable.instagram
+    };
+
+}
