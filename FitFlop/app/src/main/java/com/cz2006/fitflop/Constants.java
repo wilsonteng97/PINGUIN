89f9d48be462f916f514339020560dcdff5f429b
Wilson <wilsonteng89@gmail.com>
2020-04-09 14:50:40 +0800

    Finished Documentations for UserClient & Constants class

--- FitFlop/app/src/main/java/com/cz2006/fitflop/Constants.java
+++ FitFlop/app/src/main/java/com/cz2006/fitflop/Constants.java
@@ -1,5 +1,9 @@
 package com.cz2006.fitflop;
 
+/**
+ * The Constants class helps to identify the error surfacing
+ * from requesting User permissions in the application.
+ */
 public class Constants {
     public static final int ERROR_DIALOG_REQUEST = 9001;
     public static final int PERMISSIONS_REQUEST_ENABLE_GPS = 9002;
--- FitFlop/app/src/main/java/com/cz2006/fitflop/UserClient.java
+++ FitFlop/app/src/main/java/com/cz2006/fitflop/UserClient.java
@@ -16,6 +16,10 @@ import com.google.firebase.firestore.GeoPoint;
 
 import java.util.ArrayList;
 
+/**
+ * UserClient is a Application Instance which stores the logged-in user's information.
+ * It helps prevent unnecessary reads/writes to the Database.
+ */
 public class UserClient extends Application {
 
     private static final String TAG = "UserClient";
@@ -27,27 +31,29 @@ public class UserClient extends Application {
 
     private static ArrayList<String> facilitiesNearYou = null;
 
+    /**
+     * Retrieves the current Logged-in User.
+     * @return the logged-in User object
+     */
     public static User getUser() {
         return user;
     }
 
+    /**
+     * Sets the Logged-in User and writes the User to database if User has not been set before.
+     * @return
+     */
     public static void setUser(User givenUser) {
-        if (givenUser!=null) {
+        if (givenUser!=null){
             user = givenUser;
             saveUserToDB(user);
         }
     }
 
-    public static GeoPoint getGeoPoint() {
-//        if (geoPoint == null) return CENTER_OF_SG;
-        if (geoPoint == null) return new GeoPoint(1.3461952,103.6793612);
-        return geoPoint;
-    }
-
-    public static void setGeoPoint(GeoPoint givenGeoPoint) {
-        geoPoint = givenGeoPoint;
-    }
-
+    /**
+     * Saves the updatedUser to the Database.
+     * @param updatedUser
+     */
     private static void saveUserToDB(User updatedUser) {
         DocumentReference newUserRef = FirebaseFirestore.getInstance()
                 .collection("Users")
@@ -59,22 +65,57 @@ public class UserClient extends Application {
                 if(task.isSuccessful()){
                     Log.d(TAG, "onComplete: \ninserted user location into database.");
                 }
-                }
-            });
+            }
+        });
     }
 
+    /**
+     * Retrieves the GeoPoint Object of the current Logged-in User.
+     * @return geoPoint
+     */
+    public static GeoPoint getGeoPoint() {
+//        if (geoPoint == null) return CENTER_OF_SG;
+        if (geoPoint == null) return new GeoPoint(1.3461952,103.6793612);
+        return geoPoint;
+    }
+
+    /**
+     * Sets the GeoPoint of the current Logged-in User.
+     * @param givenGeoPoint
+     */
+    public static void setGeoPoint(GeoPoint givenGeoPoint) {
+        geoPoint = givenGeoPoint;
+    }
+
+    /**
+     * Retrieves the information of the locations near the user's current location.
+     * @return
+     */
     public static GeoJsonFeatureHashMapInfo getGeoJsonFeatureInfo() {
         return geoJsonFeatureInfo;
     }
 
+    /**
+     * Sets the information of the locations near the user.
+     * Updated when either the user's location is changed.
+     * @param givenGeoJsonFeatureInfo
+     */
     public static void setGeoJsonFeatureInfo(GeoJsonFeatureHashMapInfo givenGeoJsonFeatureInfo) {
         geoJsonFeatureInfo = givenGeoJsonFeatureInfo;
     }
 
+    /**
+     * Retrieves the name of the facilities near the user's current location.
+     * @return Names of the facilities near the user
+     */
     public static ArrayList<String> getFacilitiesNearYou() {
         return facilitiesNearYou;
     }
 
+    /**
+     * Sets name of the facilities near the user's current location.
+     * @param givenfacilitiesNearYou
+     */
     public static void setFacilitiesNearYou(ArrayList<String> givenfacilitiesNearYou) {
         facilitiesNearYou = givenfacilitiesNearYou;
     }
