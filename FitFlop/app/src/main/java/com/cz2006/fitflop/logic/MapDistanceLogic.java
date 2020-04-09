a6ef5f6fca3a58d2fcc5355a797892b127a5a16e
Wilson <wilsonteng89@gmail.com>
2020-04-09 14:17:19 +0800

    Logic Package Documentation

--- FitFlop/app/src/main/java/com/cz2006/fitflop/logic/MapDistanceLogic.java
+++ FitFlop/app/src/main/java/com/cz2006/fitflop/logic/MapDistanceLogic.java
@@ -2,9 +2,18 @@ package com.cz2006.fitflop.logic;
 
 import com.google.android.gms.maps.model.LatLng;
 
+/**
+ * Calculates the Distance between 2 locations.
+ */
 public class MapDistanceLogic {
     private static final double EARTH_RADIUS = 6371000; //meters
 
+    /**
+     * Gets the accurate distance between 2 locations.
+     * @param feature_location
+     * @param current_user_location
+     * @return distance
+     */
     public static double getDistance(LatLng feature_location, LatLng current_user_location) {
         double dist = 0;
 
@@ -12,6 +21,15 @@ public class MapDistanceLogic {
         return dist;
     }
 
+    /**
+     * Gets the distance between 2 locations.
+     * If the distance between the 2 locations is definitely less than 100m,
+     * use a faster, but more inaccurate method of calculating the distance.
+     * @param metres
+     * @param feature_location
+     * @param current_user_location
+     * @return
+     */
     public static double getDistance(double metres, LatLng feature_location, LatLng current_user_location) {
         double dist = 0;
         if (metres <= 100) {
@@ -37,10 +55,15 @@ public class MapDistanceLogic {
         double lng2 = latLng2.longitude;
 
         double a = (lat1 - lat2) * distPerLat(lat1);
-        double b = (lng1 - lng2) * distPerLng(lat1);
+        double b = (lng1 - lng2) * distPerLng(lng1);
         return Math.sqrt(a * a + b * b);
     }
 
+    /**
+     * Approximates the distance per Lat based upon the first Lat point.
+     * @param lat
+     * @return
+     */
     private static double distPerLng(double lat) {
         return 0.0003121092 * Math.pow(lat, 4)
                 + 0.0101182384 * Math.pow(lat, 3)
@@ -48,6 +71,11 @@ public class MapDistanceLogic {
                 + 5.5485277537 * lat + 111301.967182595;
     }
 
+    /**
+     * Approximates the distance per Lng based upon the first Lat point.
+     * @param lat
+     * @return
+     */
     private static double distPerLat(double lat) {
         return -0.000000487305676 * Math.pow(lat, 4)
                 - 0.0033668574 * Math.pow(lat, 3)
@@ -55,7 +83,13 @@ public class MapDistanceLogic {
                 - 1.4558127346 * lat + 110579.25662316;
     }
 
-
+    /**
+     * Calculates the Distance between 2 points after conversion to radians.
+     * Uses the Earth's radius for calculation.
+     * @param latLng1
+     * @param latLng2
+     * @return
+     */
     private static double getAccurateDist(LatLng latLng1, LatLng latLng2) {
         double lat1 = latLng1.latitude;
         double lng1 = latLng1.longitude;
